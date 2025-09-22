8b25c283fddba19a69c98b3af99bc58ead99c61f
GerVicDonar <german@donaronline.org>
2025-09-22 19:30:55 -0300

    Initial commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  require "support/factory_bot"
+  require "support/database_cleaner"
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
